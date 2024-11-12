86dc448407c90830a847f0f83b659875810b6da4
lancifer05 <gamboafreelan@gmail.com>
2024-11-12 13:24:46 +0800

    Redesign

---
+++ index.js
@@ -0,0 +1,19 @@
+// const element = document.querySelector('.rule');
+// const texts = ['Mobile Developer', 'Web Developer'];
+// let currentIndex = 0;
+
+// function changeText() {
+//   currentIndex = (currentIndex + 1) % texts.length; // Toggle between 0 and 1
+//   element.textContent = texts[currentIndex];
+
+//   // Restart typing effect by removing and re-adding the class
+//   element.classList.remove('typing');
+//   void element.offsetWidth;  // Trigger reflow
+//   element.classList.add('typing');
+// }
+
+// // Start the text change every 4 seconds
+// setInterval(changeText, 1000);
+
+// // Initial typing effect
+// element.classList.add('typing');
