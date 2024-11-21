48fa08f38c7c17d4e76656943f2f8639d3fc00a7
lancifer05 <gamboafreelan@gmail.com>
2024-11-21 19:49:14 +0800

    some add

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const switchButton = document.querySelector('.switch');
     const body = document.body;
   
-    // Check ng saved theme preference sa localStorage
+    // Check for saved theme preference in localStorage
     if (localStorage.getItem('theme') === 'light') {
       body.classList.add('lightmode');
     }
   
     // Toggle theme and save preference
     switchButton.addEventListener('click', () => {
-      body.classList.toggle('lightmode');
+      // Apply a slight delay to see the transition effect
+      setTimeout(() => {
+        body.classList.toggle('lightmode');
   
-      // Save ng theme preference in localStorage
-      if (body.classList.contains('lightmode')) {
-        localStorage.setItem('theme', 'light');
-      } else {
-        localStorage.removeItem('theme');
-      }
+        // Save the current theme preference in localStorage
+        if (body.classList.contains('lightmode')) {
+          localStorage.setItem('theme', 'light');
+        } else {
+          localStorage.removeItem('theme');
+        }
+      }, 100); // 100ms delay for a smoother transition
     });
   });
   
